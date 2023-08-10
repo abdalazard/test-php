0ed53b62bf71837155fdb7e6c7ce9f8a5afe1d7a
Abdalazard <abdalazard@gmail.com>
2023-08-10 18:45:51 -0300

    Corrige estilo

--- db/Connect.php
+++ db/Connect.php
@@ -8,6 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+    die("Erro na conexï¿½o com o banco de dados: " . $e->getMessage());
 }
 ?>
