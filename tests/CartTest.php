b1bff95142b7521ae3091393bf827c507d58a751
Abdalazard <abdalazard@gmail.com>
2023-08-10 19:42:16 -0300

    Adiciona parametro de preço

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -10,65 +10,64 @@ class CartTest extends TestCase
 {
     public function test_if_cart_is_empty()
     {
-        
+
         //Arrange
         $cart = new Cart;
-        
+
         //Act
         $result = $cart->getCart();
-        
+
         //Assert
         $this->assertEmpty($result);
     }
-    
+
     public function test_if_exist_product_in_cart()
     {
         //Arrange
         $product = new Products;
-        $product->setItem('Bermuda');
-        
+        $product->setItem('Bermuda', 10);
+
         $cart = new Cart;
         $cart->add($product);
-        
         //Act
         $result = $cart->getCart();
-        
+
         //Assert
         $this->assertCount(1, $result);
     }
-    
+
     public function test_exist_more_than_one_product_in_cart()
     {
         //Arrange
         $productA = new Products;
         $productB = new Products;
-        $productA->setItem('Bermuda');
-        $productB->setItem('Camisa');
+        $productA->setItem('Bermuda', 10);
+        $productB->setItem('Camisa', 5);
         $cart = new Cart;
         $cart->add($productA);
         $cart->add($productB);
-        
+
         //Act
         $result = $cart->getCart();
-        
-        //Assert
-        $this->assertCount(2, $result);   
-     }
-        
-        public function test_if_cart_is_clean()
-        {
-            //Arrange
-            $product = new Products;
-            $product->setItem('Bermuda');
-            $cart = new Cart;
-            $cart->add($product);
-            
-            // $this->assertCount(1, $cart->getCart());
-            
-            //Act
-            $result = $cart->clean();
 
-            //Assert
-            $this->assertCount(0, $result);
-        }
+        //Assert
+        $this->assertCount(2, $result);
     }
+
+    public function test_if_cart_is_clean()
+    {
+        //Arrange
+        $product = new Products;
+        $product->setItem('Bermuda', 20);
+        $cart = new Cart;
+        $cart->add($product);
+
+        // $this->assertCount(1, $cart->getCart());
+
+        //Act
+        $result = $cart->clean();
+
+        //Assert
+        $this->assertCount(0, $result);
+    }
+}
